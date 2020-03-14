c0d41feac395d6ec658444bc6746b764a6dfd6e3
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-14 15:44:30 +0530

    ADDED:Method To Find Maximum Value Between Three Integer

--- src/main/java/MaximumValue.java
+++ src/main/java/MaximumValue.java
@@ -2,4 +2,11 @@ public class MaximumValue {
     public static void main(String[] args) {
         System.out.println("Welcome To Find Maximum Value Problem");
     }
+
+    //METHOD TO FIND MAXIMUM INTEGER
+    public int getMaximumNumber(int firstNumber, int secondNumber, int thirdNumber) {
+        int maximumResultOne = Math.max(firstNumber, secondNumber);
+        int maximumResult = Math.max(thirdNumber, maximumResultOne);
+        return maximumResult;
+    }
 }
