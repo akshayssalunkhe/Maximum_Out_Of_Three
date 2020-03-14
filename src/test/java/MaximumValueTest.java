36afbc76c3659f7a57deabc7f7efc2048d2f0af9
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-14 15:57:42 +0530

    ADDED:Test Case To Check If Second Number Is Maximum

--- src/test/java/MaximumValueTest.java
+++ src/test/java/MaximumValueTest.java
@@ -2,10 +2,18 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MaximumValueTest {
+    //GLOBAL OBJECT
+    MaximumValue maximumValue = new MaximumValue();
+
     @Test
     public void givenThreeInteger_WhenFirstIsMaximum_ShouldReturnMaximumNumber() {
-        MaximumValue maximumValue = new MaximumValue();
         int resultMaximumNumber = maximumValue.getMaximumNumber(16, 5, 10);
+        Assert.assertEquals(16, resultMaximumNumber);
+    }
+
+    @Test
+    public void givenThreeInteger_WhenSecondNumberIsMaximum_ShouldReturnMaximumNumber() {
+        int resultMaximumNumber = maximumValue.getMaximumNumber(5, 16, 10);
         Assert.assertEquals(16, resultMaximumNumber);
     }
 }
