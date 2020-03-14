7c92119a5a1d94d8b1de9d595cb50b715380d93a
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-14 15:48:43 +0530

    ADDED:Test Case For If First Number Is Maximum

--- src/test/java/MaximumValueTest.java
+++ src/test/java/MaximumValueTest.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MaximumValueTest {
+    @Test
+    public void givenThreeInteger_WhenFirstIsMaximum_ShouldReturnMaximumNumber() {
+        MaximumValue maximumValue = new MaximumValue();
+        int resultMaximumNumber = maximumValue.getMaximumNumber(16, 5, 10);
+        Assert.assertEquals(16, resultMaximumNumber);
+    }
 }
