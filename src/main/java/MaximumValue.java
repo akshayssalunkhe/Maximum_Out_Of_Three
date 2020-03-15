42ccbcd4725e201d14c1e087445e4b9905b23c68
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-15 12:10:25 +0530

    ADDED:Method To Find Maximum Float Value Between Three Values

--- src/main/java/MaximumValue.java
+++ src/main/java/MaximumValue.java
@@ -3,10 +3,17 @@ public class MaximumValue {
         System.out.println("Welcome To Find Maximum Value Problem");
     }
 
-    //METHOD TO FIND MAXIMUM INTEGER
+    //METHOD TO FIND MAXIMUM INTEGER VALUE
     public int getMaximumNumber(int firstNumber, int secondNumber, int thirdNumber) {
         int maximumResultOne = Math.max(firstNumber, secondNumber);
         int maximumResult = Math.max(thirdNumber, maximumResultOne);
         return maximumResult;
+    }
+
+    //METHOD TO FIND MAXIMUM FLOAT VALUE
+    public float getMaximumFloatNumber(float firstNumber, float secondNumber, float thirdNumber) {
+        float maximumResultOne = Math.max(firstNumber, secondNumber);
+        float maximumResultFloat = Math.max(thirdNumber, maximumResultOne);
+        return maximumResultFloat;
     }
 }
