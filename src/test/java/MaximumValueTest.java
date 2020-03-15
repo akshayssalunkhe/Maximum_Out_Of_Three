732b2cf4d3b284968cb1e6dd19ea6f7e52e16b93
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-15 11:06:42 +0530

    ADDED:Test Case To Check If Third Number Is Maximum

--- src/test/java/MaximumValueTest.java
+++ src/test/java/MaximumValueTest.java
@@ -6,7 +6,7 @@ public class MaximumValueTest {
     MaximumValue maximumValue = new MaximumValue();
 
     @Test
-    public void givenThreeInteger_WhenFirstIsMaximum_ShouldReturnMaximumNumber() {
+    public void givenThreeInteger_WhenFirstNumberIsMaximum_ShouldReturnMaximumNumber() {
         int resultMaximumNumber = maximumValue.getMaximumNumber(16, 5, 10);
         Assert.assertEquals(16, resultMaximumNumber);
     }
@@ -15,5 +15,11 @@ public class MaximumValueTest {
     public void givenThreeInteger_WhenSecondNumberIsMaximum_ShouldReturnMaximumNumber() {
         int resultMaximumNumber = maximumValue.getMaximumNumber(5, 16, 10);
         Assert.assertEquals(16, resultMaximumNumber);
+    }
+
+    @Test
+    public void givenThreeInteger_WhenThirdNumberIsMaximum_ShouldReturnMaximumNumber() {
+        int resultMaximumNumber = maximumValue.getMaximumNumber( 5, 10, 16);
+        Assert.assertEquals(16,resultMaximumNumber);
     }
 }
