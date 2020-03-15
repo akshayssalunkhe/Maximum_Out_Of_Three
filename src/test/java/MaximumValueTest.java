eca38585021ff6afc2dfe53f57e02a9a9bb75506
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-15 16:50:27 +0530

    REFACTORED:All Test Cases Calling One Generic Method

--- src/test/java/MaximumValueTest.java
+++ src/test/java/MaximumValueTest.java
@@ -7,73 +7,73 @@ public class MaximumValueTest {
 
     @Test
     public void givenThreeInteger_WhenFirstNumberIsMaximum_ShouldReturnMaximumNumber() {
-        int resultMaximumNumber = maximumValue.getMaximumNumber(16, 5, 10);
+        int resultMaximumNumber = maximumValue.getMaximumValue(16, 5, 10);
         Assert.assertEquals(16, resultMaximumNumber);
     }
 
     @Test
     public void givenThreeInteger_WhenSecondNumberIsMaximum_ShouldReturnMaximumNumber() {
-        int resultMaximumNumber = maximumValue.getMaximumNumber(5, 16, 10);
+        int resultMaximumNumber = maximumValue.getMaximumValue(5, 16, 10);
         Assert.assertEquals(16, resultMaximumNumber);
     }
 
     @Test
     public void givenThreeInteger_WhenThirdNumberIsMaximum_ShouldReturnMaximumNumber() {
-        int resultMaximumNumber = maximumValue.getMaximumNumber(5, 10, 16);
+        int resultMaximumNumber = maximumValue.getMaximumValue(5, 10, 16);
         Assert.assertEquals(16, resultMaximumNumber);
     }
 
     @Test
     public void givenThreeInteger_WhenTwoNumbersAreEqual_ShouldReturnMaximumNumber_() {
-        int resultMaximumNumber = maximumValue.getMaximumNumber(16, 10, 16);
+        int resultMaximumNumber = maximumValue.getMaximumValue(16, 10, 16);
         Assert.assertEquals(16, resultMaximumNumber);
     }
 
     @Test
     public void givenThreeFloat_WhenFirstNumberIsMaximum_ShouldReturnMaximumNumber() {
-        float resultMaximumNumber = maximumValue.getMaximumFloatNumber(16.5f, 10.55f, 5.1f);
+        float resultMaximumNumber = maximumValue.getMaximumValue(16.5f, 10.55f, 5.1f);
         Assert.assertEquals(16.5, resultMaximumNumber, 0.0);
     }
 
     @Test
     public void givenThreeFloat_WhenSecondNumberIsMaximum_ShouldReturnMaximumNumber() {
-        float resultMaximumNumber = maximumValue.getMaximumFloatNumber(10.55f, 16.5f, 5.1f);
+        float resultMaximumNumber = maximumValue.getMaximumValue(10.55f, 16.5f, 5.1f);
         Assert.assertEquals(16.5, resultMaximumNumber, 0.0);
     }
 
     @Test
     public void givenThreeFloat_WhenThirdNumberIsMaximum_ShouldReturnMaximumNumber() {
-        float resultMaximumNumber = maximumValue.getMaximumFloatNumber(10.55f, 5.1f, 16.5f);
+        float resultMaximumNumber = maximumValue.getMaximumValue(10.55f, 5.1f, 16.5f);
         Assert.assertEquals(16.5, resultMaximumNumber, 0.0);
     }
 
     @Test
     public void givenThreeFloat_WhenTwoNumberAreEqual_ShouldReturnMaximumNumber() {
-        float resultMaximumNumber = maximumValue.getMaximumFloatNumber(10.5f, 16.5f, 10.5f);
+        float resultMaximumNumber = maximumValue.getMaximumValue(10.5f, 16.5f, 10.5f);
         Assert.assertEquals(16.5, resultMaximumNumber, 0.0);
     }
 
     @Test
     public void givenThreeStrings_WhenFirstIsMaximumString_ShouldReturnMaximumString() {
-        String resultMaximumString = maximumValue.getMaximumString("Amey", "Amar", "Aman");
+        String resultMaximumString = maximumValue.getMaximumValue("Amey", "Amar", "Aman");
         Assert.assertEquals("Amey", resultMaximumString);
     }
 
     @Test
     public void givenThreeStrings_WhenSecondIsMaximumString_ShouldReturnMaximumString() {
-        String resultMaximumString = maximumValue.getMaximumString("Amar", "Amey", "Aman");
+        String resultMaximumString = maximumValue.getMaximumValue("Amar", "Amey", "Aman");
         Assert.assertEquals("Amey", resultMaximumString);
     }
 
     @Test
     public void givenThreeStrings_WhenThirdIsMaximumString_ShouldReturnMaximumString() {
-        String resultMaximumString = maximumValue.getMaximumString("Amar", "Aman", "Amey");
+        String resultMaximumString = maximumValue.getMaximumValue("Amar", "Aman", "Amey");
         Assert.assertEquals("Amey", resultMaximumString);
     }
 
     @Test
     public void givenThreeStrings_WhenTwoStringsAreEqual_ShouldReturnMaximumString() {
-        String resultMaximumString = maximumValue.getMaximumString("Amey", "Aman", "Amey");
+        String resultMaximumString = maximumValue.getMaximumValue("Amey", "Aman", "Amey");
         Assert.assertEquals("Amey", resultMaximumString);
     }
 }
