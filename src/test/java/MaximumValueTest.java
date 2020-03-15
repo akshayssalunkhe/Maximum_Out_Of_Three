6a562877eb31d439a603c7ec0efbd6f83bf2bb1d
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-15 12:13:24 +0530

    ADDED:Test Case To Check If First Value Is Maximum Then Should Return Maximum

--- src/test/java/MaximumValueTest.java
+++ src/test/java/MaximumValueTest.java
@@ -19,13 +19,19 @@ public class MaximumValueTest {
 
     @Test
     public void givenThreeInteger_WhenThirdNumberIsMaximum_ShouldReturnMaximumNumber() {
-        int resultMaximumNumber = maximumValue.getMaximumNumber( 5, 10, 16);
-        Assert.assertEquals(16,resultMaximumNumber);
+        int resultMaximumNumber = maximumValue.getMaximumNumber(5, 10, 16);
+        Assert.assertEquals(16, resultMaximumNumber);
     }
 
     @Test
     public void givenThreeInteger_WhenTwoNumbersAreEqual_ShouldReturnMaximumNumber_() {
-        int resultMaximumNumber = maximumValue.getMaximumNumber(16, 10 ,16);
-        Assert.assertEquals(16,resultMaximumNumber);
+        int resultMaximumNumber = maximumValue.getMaximumNumber(16, 10, 16);
+        Assert.assertEquals(16, resultMaximumNumber);
+    }
+
+    @Test
+    public void givenThreeFloat_WhenFirstNumberIsMaximum_ShouldReturnMaximumNumber() {
+        float resultMaximumNumber = maximumValue.getMaximumFloatNumber(16.5f, 10.55f, 5.1f);
+        Assert.assertEquals(16.5, resultMaximumNumber, 0.0);
     }
 }
